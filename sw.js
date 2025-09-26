3bdd2cb98bb91e40bdd232e1f16a03e7906e3291
Arellano136 <132949146+Arellano136@users.noreply.github.com>
2025-09-26 05:31:13 -0600

    [ADD] index.html y sw.js subido

---
+++ sw.js
@@ -0,0 +1,27 @@
+// sw.js
+self.addEventListener("install", (event) => {
+  sendMessage("Instalada");
+  self.skipWaiting(); // pasa a waiting
+});
+
+self.addEventListener("activate", (event) => {
+  sendMessage("Activación");
+  event.waitUntil(clients.claim()); // lo hace activo inmediatamente
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data === "checkIdle") {
+    sendMessage("Ocioso");
+  }
+});
+
+// Función para enviar mensajes al cliente (páginas)
+function sendMessage(status) {
+  const now = new Date();
+  const timestamp = now.toISOString() + `.${now.getMilliseconds()}`;
+  self.clients.matchAll().then((clients) => {
+    clients.forEach((client) => {
+      client.postMessage({ status, timestamp });
+    });
+  });
+}
